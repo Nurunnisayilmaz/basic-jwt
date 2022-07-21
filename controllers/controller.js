ac882428d6586005645a9c30dce6190d72a2b506
nisa <nurunnisa9@gmail.com>
2022-07-21 11:06:40 +0300

    created login in controller

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,5 @@
 const User = require('../model/userModel');
+const jwt = require('jsonwebtoken');
 
 
 const register = async (req, res) => {
@@ -16,7 +17,21 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
+    const {name,password} = req.body;
     try {
+        const data = await User.findOne();
+
+        if(data===0){
+            return res.status(404).json({message: "Not Found."});
+        }
+
+            const token = jwt.sign({
+
+                name : name,
+                id : data._id.toString()
+
+            },'secretKey')
+
         return res.status(200).json({status: 'success'})
     } catch (e) {
         return res.status(404).json({code: 404, message: "Not Found", error: e})
